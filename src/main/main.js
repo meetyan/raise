fcc1beebb1a431bb0a5c019957867851f777f93a
Jiajun Yan <yanjiajun@ifanr.com>
2022-07-28 18:14:07 +0800

    feat: Added styles to index page and added support for dark mode

--- src/main/main.js
+++ src/main/main.js
@@ -1,4 +1,4 @@
-import {app} from 'electron'
+import {app, BrowserWindow} from 'electron'
 import path from 'path'
 import isDev from 'electron-is-dev'
 import {menubar} from 'menubar'
@@ -8,12 +8,30 @@ const INDEX_URL = {
   PROD: path.join(__dirname, './index.html'),
 }
 
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 400,
+    height: 600,
+  })
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools({mode: 'detach'})
+    return mainWindow.loadURL(INDEX_URL.DEV)
+  }
+
+  mainWindow.loadFile(INDEX_URL.PROD)
+}
+
 app.whenReady().then(() => {
   const mb = menubar({
     icon: path.join(__dirname, './assets/logo.png'),
     index: isDev ? INDEX_URL.DEV : INDEX_URL.PROD,
-    browserWindow: {width: 400, height: 400},
+    browserWindow: {width: 400, height: 600, webPreferences: {devTools: true}},
   })
+
+  if (isDev) {
+    createWindow()
+  }
 
   mb.on('ready', () => {})
 })
--- src/renderer/app.js
+++ src/renderer/app.js
@@ -1,9 +1,172 @@
 import React from 'react'
+import {Button, Layout, Form, Divider, Card, Typography, Space, BackTop} from '@douyinfe/semi-ui'
+import {
+  IconArrowUp,
+  IconBranch,
+  IconGithubLogo,
+  IconMoon,
+  IconSetting,
+  IconSourceControl,
+  IconStar,
+} from '@douyinfe/semi-icons'
+import sample from '@/sample'
 
-const App = () => (
-  <div>
-    <h1>Hello React111</h1>
-  </div>
-)
+import '@/assets/styles/reset.scss'
+import '@/assets/styles/global.scss'
+
+import styles from '@/app.scss'
+
+const App = () => {
+  const {Header, Footer, Content} = Layout
+
+  const switchMode = () => {
+    const body = document.body
+    if (body.hasAttribute('theme-mode')) {
+      body.removeAttribute('theme-mode')
+    } else {
+      body.setAttribute('theme-mode', 'dark')
+    }
+  }
+
+  return (
+    <Layout className={`components-layout-demo ${styles.layout}`}>
+      <Header className={styles.trendingType}>
+        <div className={styles.top}>
+          <h1 className={styles.heading}>
+            <IconGithubLogo />
+            <Typography.Text className={styles.headingTitle}>Trending</Typography.Text>
+          </h1>
+
+          <div className={styles.buttonType}>
+            <Button type="primary" theme="solid" className={styles.trendingTypeButton}>
+              Repositories
+            </Button>
+            <Button className={styles.trendingTypeButton}>Developers</Button>
+          </div>
+        </div>
+
+        <div className={styles.middle}>
+          <Button theme="borderless" icon={<IconMoon />} onClick={switchMode} />
+          <Button theme="borderless" icon={<IconSetting />} />
+        </div>
+
+        <Divider />
+
+        <div className={styles.bottom}>
+          <Form labelPosition="left" labelAlign="left" labelWidth={180}>
+            <Form.Select
+              field="spokenLanguage"
+              initValue="english"
+              label="Spoken language"
+              className={styles.bottomSelect}
+              filter
+            >
+              <Form.Select.Option value="english">English</Form.Select.Option>
+              <Form.Select.Option value="chinese">Chinese</Form.Select.Option>
+              {Array.from({length: 20}, (_, idx) => `${++idx}`).map(item => {
+                return (
+                  <Form.Select.Option key={item} value={item}>
+                    {item}
+                  </Form.Select.Option>
+                )
+              })}
+            </Form.Select>
+
+            <Form.Select
+              field="language"
+              initValue="javascript"
+              label="Language"
+              className={styles.bottomSelect}
+              filter
+            >
+              <Form.Select.Option value="javascript">Javascript</Form.Select.Option>
+              <Form.Select.Option value="python">Python</Form.Select.Option>
+            </Form.Select>
+
+            <Form.Select
+              field="dateRange"
+              initValue="today"
+              label="Date range"
+              className={styles.bottomSelect}
+              filter
+            >
+              <Form.Select.Option value="today">Today</Form.Select.Option>
+              <Form.Select.Option value="this-week">This week</Form.Select.Option>
+              <Form.Select.Option value="this-month">This month</Form.Select.Option>
+            </Form.Select>
+          </Form>
+        </div>
+
+        <Divider />
+      </Header>
+
+      <Content className={styles.content}>
+        {sample.map(item => {
+          return (
+            <Card
+              key={item.repo}
+              title={
+                <div className={styles.repoHeader}>
+                  <IconBranch />
+                  <div className={styles.repoAuthor}>
+                    <Typography.Text>{item.author}</Typography.Text> /{' '}
+                    <Typography.Text strong>{item.repo}</Typography.Text>
+                  </div>
+                </div>
+              }
+              className={styles.repo}
+              headerExtraContent={
+                <Typography.Text type="secondary">{item.language}</Typography.Text>
+              }
+            >
+              <Typography.Text className={styles.description}>{item.description}</Typography.Text>
+
+              <div className={styles.bottomArea}>
+                <div className={styles.top}>
+                  <Space>
+                    <Space>
+                      <IconStar /> <Typography.Text>{item.stars}</Typography.Text>
+                    </Space>
+                    <Space />
+                    <Space>
+                      <IconSourceControl /> <Typography.Text>{item.forked}</Typography.Text>
+                    </Space>
+                  </Space>
+
+                  <Space>
+                    <IconStar /> <Typography.Text>{item.starsToday} stars today</Typography.Text>
+                  </Space>
+                </div>
+
+                <div className={styles.bottom}>
+                  <Space>
+                    <Typography.Text>Built by</Typography.Text>
+                    <div>
+                      {item.builtBy.map(builtByAuthor => {
+                        return (
+                          <img
+                            className={styles.avatar}
+                            src={builtByAuthor.avatar}
+                            key={builtByAuthor.avatar}
+                          />
+                        )
+                      })}
+                    </div>
+                  </Space>
+                </div>
+              </div>
+            </Card>
+          )
+        })}
+      </Content>
+
+      <Footer>Raise</Footer>
+
+      <BackTop className={styles.backTop}>
+        <IconArrowUp />
+      </BackTop>
+    </Layout>
+  )
+}
 
 export default App
