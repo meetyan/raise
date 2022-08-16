adce4e1fe2e684b78a0d3d6d64785d243911ae54
Jiajun Yan <yanjiajun@ifanr.com>
2022-08-16 18:11:03 +0800

    feat: Added is-electron

---
+++ src/lib/index.js
@@ -0,0 +1,1 @@
+export {default as isElectron} from './is-electron'
---
+++ src/lib/is-electron.js
@@ -0,0 +1,32 @@
+const isElectron = () => {
+  // Renderer process
+  if (
+    typeof window !== 'undefined' &&
+    typeof window.process === 'object' &&
+    window.process.type === 'renderer'
+  ) {
+    return true
+  }
+
+  // Main process
+  if (
+    typeof process !== 'undefined' &&
+    typeof process.versions === 'object' &&
+    !!process.versions.electron
+  ) {
+    return true
+  }
+
+  // Detect the user agent when the `nodeIntegration` option is set to false
+  if (
+    typeof navigator === 'object' &&
+    typeof navigator.userAgent === 'string' &&
+    navigator.userAgent.indexOf('Electron') >= 0
+  ) {
+    return true
+  }
+
+  return false
+}
+
+export default isElectron
